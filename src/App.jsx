b340c3dbe74e0bdc696ba996771eb4678e322742
Lemmi, Guilherme <guilherme.lemmi@vistajet.com>
2018-02-10 19:55:41 -0200

    basic wireframing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
-import StarWarsTopTrumps from './components/StarWarsTopTrumps';
 import './App.css';
 
-const App = () => <StarWarsTopTrumps />;
+const App = () => (
+  <div className="star-wars-top-trumps">
+    <div className="score-panel">
+      <div className="player-scores">
+        <div className="player-score">Player Score</div>
+        <div className="player-score">Player Score</div>
+      </div>
+      <div className="result">Result</div>
+    </div>
+    <div className="game-panel">
+      <div className="card-container">
+        <div className="card">
+          <div className="card-image">Card Image</div>
+          <div className="card-feature-list">Card Feature List</div>
+        </div>
+        <div className="selected-feature">Selected Feature</div>
+      </div>
+      <div className="game-panel-x">x</div>
+      <div className="card-container">
+        <div className="card">
+          <div className="card-image">Card Image</div>
+          <div className="card-feature-list">Card Feature List</div>
+        </div>
+        <div className="selected-feature">Selected Feature</div>
+      </div>
+    </div>
+    <div className="control-panel">
+      <div className="button">Button</div>
+    </div>
+  </div>
+);
 
 export default App;
--- src/components/StarWarsTopTrumps.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const StarWarsTopTrumps = () => <div>Hello World!</div>;
-
-export default StarWarsTopTrumps;
