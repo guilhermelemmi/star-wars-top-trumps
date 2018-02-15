0e0aaf1bf260ad4a0d7bd516128e61cc891a0f05
Lemmi, Guilherme <guilherme.lemmi@vistajet.com>
2018-02-15 00:36:47 -0200

    basic html and css static mock

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,66 @@
 import React from 'react';
 import './App.css';
+import catamaran from './images/Catamaran.jpg';
 
 const App = () => (
   <div className="star-wars-top-trumps">
     <div className="score-panel">
       <div className="player-scores">
-        <div className="player-score">Player Score</div>
-        <div className="player-score">Player Score</div>
+        <div><span className="player-score">11</span></div>
+        <div><span className="player-score">28</span></div>
       </div>
-      <div className="result">Result</div>
     </div>
     <div className="game-panel">
-      <div className="card-container">
+      <div className="game-panel-result">Draw</div>
+      <div className="card-container winner">
+        <div className="card-result">Winner</div>
         <div className="card">
-          <div className="card-image">Card Image</div>
-          <div className="card-feature-list">Card Feature List</div>
+          <div className="card-header">
+            <h3>Raddaugh Gnasp fluttercraft</h3>
+            <div className="card-additional-info">
+              <div>Model: Raddaugh Gnasp fluttercraft</div>
+              <div>Manufacturer: Appazanna Engineering Works</div>
+            </div>
+          </div>
+          <div className="card-image-container">
+            <img src={catamaran} className="card-image" alt="" />
+          </div>
+          <div className="card-feature-list">
+            <div className="card-feature-row">
+              <span className="card-feature-label">Cost:</span>
+              <span className="card-feature-value">14750</span>
+            </div>
+            <div className="card-feature-row">
+              <span className="card-feature-label">Length:</span>
+              <span className="card-feature-value">7</span>
+            </div>
+            <div className="card-feature-row">
+              <span className="card-feature-label">Speed:</span>
+              <span className="card-feature-value">310</span>
+            </div>
+            <div className="card-feature-row">
+              <span className="card-feature-label">Crew:</span>
+              <span className="card-feature-value">2</span>
+            </div>
+            <div className="card-feature-row">
+              <span className="card-feature-label">Passengers:</span>
+              <span className="card-feature-value">0</span>
+            </div>
+            <div className="card-feature-row">
+              <span className="card-feature-label">Cargo Capacity:</span>
+              <span className="card-feature-value">20</span>
+            </div>
+          </div>
         </div>
-        <div className="selected-feature">Selected Feature</div>
+        <div className="selected-feature">Choose a feature</div>
       </div>
       <div className="game-panel-x">x</div>
       <div className="card-container">
-        <div className="card">
-          <div className="card-image">Card Image</div>
-          <div className="card-feature-list">Card Feature List</div>
-        </div>
-        <div className="selected-feature">Selected Feature</div>
+        <div className="card closed" />
       </div>
     </div>
     <div className="control-panel">
-      <div className="button">Button</div>
+      <a className="btn orange">Play</a>
     </div>
   </div>
 );
